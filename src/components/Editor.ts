3f8a668d076509b39bdf5600f130c652e71cea6f
Yuta Hashimoto <yuta@YutanoMacBook-Air.local>
2020-03-29 17:23:30 +0900

    just style change

--- src/components/Editor.ts
+++ src/components/Editor.ts
@@ -4,29 +4,27 @@ import BaseComponent from './BaseComponent';
 import store from '../store';
 
 export default class Editor extends BaseComponent {
+  $el: HTMLElement;
+  lineIndicatorHeight = 30;
 
-    $el: HTMLElement;
+  constructor(parentEl: HTMLElement, src = '') {
+    super();
+    this.$el = document.createElement('div');
+    this.$el.classList.add('koji-editor');
+    this.add(new HiddenTextArea());
+    this.add(new Display());
+    parentEl.appendChild(this.$el);
+    parentEl.addEventListener('resize', () => this.resize());
+    this.resize();
+    store.SET_REQUESTED_SRC(src);
+  }
 
-    constructor(parentEl: HTMLElement, src = "") {
-        super();
-        this.$el = document.createElement("div");
-        this.$el.classList.add('koji-editor');
-        this.add(new HiddenTextArea());
-        this.add(new Display());
-        parentEl.appendChild(this.$el);
-        parentEl.addEventListener("resize", () => this.resize());
-        this.resize();
-        store.SET_REQUESTED_SRC(src);
-    }
-
-    resize() {
-        const parent = this.$el.parentElement;
-        const width = parent?.clientWidth || 600;
-        const height = parent?.clientHeight || 600;
-        this.setSize(width, height);
-        console.log(width, height);
-        store.SET_EDITOR_SIZE(width, height);
-    }
-
-
+  resize() {
+    const parent = this.$el.parentElement;
+    const width = parent!.clientWidth || 600;
+    const height = parent!.clientHeight - this.lineIndicatorHeight || 600;
+    this.setSize(width, height);
+    console.log(width, height);
+    store.SET_EDITOR_SIZE(width, height);
+  }
 }
