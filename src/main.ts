cfb496b7ea638bef80d54b026f0979c316e650d9
Yuta Hashimoto <yuta@YutanoMac-mini.local>
2020-04-21 12:47:12 +0900

    removed test for worker

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
 import "./style.scss";
 export { default as Editor } from './components/Editor';
 
-const KojiWorker = require('worker-loader?inline!./koji-lang.worker')
-const worker = new KojiWorker()
-worker.postMessage({ type: "parse", src: 'ほげ' })
-
-worker.onmessage = (event: any) => {
-    console.log(event)
-}
